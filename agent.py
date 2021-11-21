08e945e046759231eefbbec736cf68fd27334684
Kavya Kopparapu <kavyakvk@gmail.com>
2021-11-21 16:55:34 -0500

    changes

--- agent.py
+++ agent.py
@@ -25,3 +25,14 @@ class Agent():
 class RandomAgent(Agent):
     def get_action(self, observation, valid_movements):
         return random.choice(valid_movements), random.random()
+
+class DQNAgent(Agent):
+    def __init__(self, id, env_params):
+        self.env_params = self.env_params
+        obs_window = self.env_params['observation_radius']*2+1
+        self.input_shape = (obs_window, obs_window)
+        super(self)
+    
+    def get_action(self, observation, valid_movements):
+        agent_static_grid, dynamic_grid = utils.process_grids(observation)
+
--- utils.py
+++ utils.py
@@ -3,15 +3,15 @@ import numpy as np
 def process_grids(observation, env_params):
     agent_grid, static_grid, dynamic_grid = observation
     agent_static_grid_processed, dynamic_grid_processed = np.array((N, M, 3)), np.array((N, M, 3))
-    for i in self.env_params.N:
-        for j in self.env_params.M:
+    for i in env_params.N:
+        for j in env_params.M:
             # combine agent and static grids
-            if static_grid[i,j] == self.env_params['coding_dict']['empty'] and agent_grid[i,j] == self.env_params['coding_dict']['agent']:
-                agent_static_grid_processed[i,j] = self.env_params['rgb_coding'][self.env_params['coding_dict']['agent']]
+            if static_grid[i,j] == env_params['coding_dict']['empty'] and agent_grid[i,j] == env_params['coding_dict']['agent']:
+                agent_static_grid_processed[i,j] = env_params['rgb_coding'][env_params['coding_dict']['agent']]
             else:
-                agent_static_grid_processed[i,j] = self.env_params['rgb_coding'][static_grid[i,j]]
+                agent_static_grid_processed[i,j] = env_params['rgb_coding'][static_grid[i,j]]
             # compute greyscale values for dynamic grid
-            greyscale_val = 255 * (self.env_params['pheromone']['cap'] - dynamic_grid[i,j])/self.env_params['pheromone']['cap']
+            greyscale_val = 255 * (env_params['pheromone']['cap'] - dynamic_grid[i,j])/env_params['pheromone']['cap']
             assert(greyscale_val > 0 and greyscale_val <= 255)
             dynamic_grid_processed = [greyscale_val]*3
     return (agent_static_grid_processed, dynamic_grid_processed)
