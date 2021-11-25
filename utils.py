f71f5406300646321d685bd2d6420a445bb68614
Lucy Liu <lucy12liu@gmail.com>
2021-11-25 14:56:23 -0500

    utils bugfixes

--- utils.py
+++ utils.py
@@ -5,17 +5,19 @@ import copy
 
 def process_grids(observation, env_params, visual=True):
     agent_grid, static_grid, dynamic_grid = observation
+    N = env_params['N']
+    M = env_params['M']
     if visual:
-        agent_static_grid_processed, dynamic_grid_processed = np.array((N, M, 3)), np.array((N, M, 3))
+        agent_static_grid_processed, dynamic_grid_processed = np.zeros((N, M, 3)), np.zeros((N, M, 3))
     else:
-        agent_static_grid_processed, dynamic_grid_processed = np.array((3, N, M)), np.array((3, N, M))
+        agent_static_grid_processed, dynamic_grid_processed = np.zeros((3, N, M)), np.zeros((3, N, M))
     
     def set_channel(a, b, grid, color):
         for channel in range(3):
             grid[channel][a][b] = color[channel]
     
-    for i in env_params.N:
-        for j in env_params.M:
+    for i in range(N):
+        for j in range(M):
             # combine agent and static grids
             if static_grid[i,j] == env_params['coding_dict']['empty'] and agent_grid[i,j] == env_params['coding_dict']['agent']:
                 if visual:
@@ -31,7 +33,7 @@ def process_grids(observation, env_params, visual=True):
             greyscale_val = 255 * (env_params['pheromone']['cap'] - dynamic_grid[i,j])/env_params['pheromone']['cap']
             assert(greyscale_val > 0 and greyscale_val <= 255)
             if visual:
-                dynamic_grid_processed = [greyscale_val]*3
+                dynamic_grid_processed[i][j] = [greyscale_val]*3
             else:
                 set_channel(i, j, dynamic_grid_processed, [greyscale_val]*3)
     return (agent_static_grid_processed, dynamic_grid_processed)
