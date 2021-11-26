7621557631e4e0f065d55ecd733a768ff45d7780
Lucy Liu <lucy12liu@gmail.com>
2021-11-26 11:31:55 -0500

    rendering and working SwarmAgent

--- agent.py
+++ agent.py
@@ -33,59 +33,69 @@ class SwarmAgent(Agent):
         self.spt = spt
         self.orientation = None
         self.obs_rad = self.env_params['observation_radius']
-        obs_window = self.env_params['observation_radius']*2+1
+        self.obs_window = self.env_params['observation_radius']*2+1
         super().__init__(id)
+        
+    def flatten(self, obs_r, obs_c):
+        return self.obs_window * r + c
+    
+    def unflatten(self, obs_pos):
+        return (obs_pos // self.obs_window, obs_pos % self.obs_window)
         
     def get_action(self, observation, valid_movements, fwd_only = 1):
         agent_grid, static_grid, dynamic_grid = observation
+        d = dynamic_grid.copy()
         last_action = tuple(np.array(self.location) - np.array(self.prev_location))
         if self.food:
             # choose direction with BFS
-            next_movement = np.random.choice(spt[self.x][self.y][0])
+            lay_pheromone = self.env_params['pheromone']['step_if_food']
+            good_actions = self.spt[self.location[0]][self.location[1]]
+            next_movement = good_actions[np.random.randint(len(good_actions))]
         
         else:
-            # head toward high pheromone
+            lay_pheromone = self.env_params['pheromone']['step']
+            # move forward, probs weighted by pheromone strength
             if last_action == (-1,0):
-                d = dynamic_grid[:self.obs_rad + 1 - fwd_only, :]
+                d[self.obs_rad + 1 - fwd_only:, :] = 0
 
             elif last_action == (0,1):
-                d = dynamic_grid[:, self.obs_rad + fwd_only:]
+                d[:, :self.obs_rad + fwd_only] = 0
 
             elif last_action == (1,0):
-                d = dynamic_grid[self.obs_rad + fwd_only:, :]
+                d[:self.obs_rad + fwd_only, :] = 0
 
             elif last_action == (0,-1):
-                d = dynamic_grid[:, :self.obs_rad + 1 - fwd_only]
-            else: 
-                d = dynamic_grid
+                d[:, self.obs_rad + 1 - fwd_only:] = 0
         
-#         best_loc = np.unravel_index(d.argmax(), d.shape)
-        best_loc = np.argwhere(d == np.max(d))
-        best_loc = best_loc[np.random.randint(len(best_loc))]
+            dflat = d.flatten()
+            if np.sum(dflat) == 0:
+                probs = [1/len(dflat)] * len(dflat)
+            else:
+                probs = dflat/np.sum(dflat)
+
+            # choose random target location in fwd direction, weighted by pheromone
+            best_loc = np.random.choice(np.arange(len(dflat)), p = probs)
+            best_loc = self.unflatten(best_loc)
                 
-        good_actions = []     
-        # if best action row > agent row in observation window
-        if best_loc[0] > self.obs_rad:
-            good_actions.append((1,0))
-        if best_loc[0] < self.obs_rad:
-            good_actions.append((-1,0))
-        if best_loc[1] > self.obs_rad:
-            good_actions.append((0,1))
-        if best_loc[1] < self.obs_rad:
-            good_actions.append((0,-1))
+            good_actions = []     
+            # if best action row > agent row in observation window
+            if best_loc[0] > self.obs_rad:
+                good_actions.append((1,0))
+            if best_loc[0] < self.obs_rad:
+                good_actions.append((-1,0))
+            if best_loc[1] > self.obs_rad:
+                good_actions.append((0,1))
+            if best_loc[1] < self.obs_rad:
+                good_actions.append((0,-1))
 
-        # if list empty take random action from all actions not blocked by obstacles
-        good_actions = list(set(good_actions).intersection(set(valid_movements)))
-        if len(good_actions) > 0:
-            next_movement = good_actions[np.random.randint(len(good_actions))]
-        else:
-            next_movement = valid_movements[np.random.randint(len(valid_movements))]
-            
-#         if self.id == 0:
-#             print(dynamic_grid)
-#             print(self.location)
-        return next_movement, self.env_params['pheromone']['step']
+            # if list empty take random action from all actions not blocked by obstacles
+            good_actions = list(set(good_actions).intersection(set(valid_movements)))
+            if len(good_actions) > 0:
+                next_movement = good_actions[np.random.randint(len(good_actions))]
+            else:
+                next_movement = valid_movements[np.random.randint(len(valid_movements))]
 
+        return next_movement, lay_pheromone
 
 class DQNAgent(Agent):
     def __init__(self, id, env_params):
